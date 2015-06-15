b68f8c99af9881f2a9c5fd96e3aad87daa615e30
Jeremy Goss <jem@goss-family.net>
2015-06-15 17:39:22 -0400

    Slightly better way of handling board revisions.

--- quick2wire/board_revision.py
+++ quick2wire/board_revision.py
@@ -1,19 +1,20 @@
 def revision():
+    """Returns a board revision number which corresponds to a partuclar
+    change in compatibility for the functions in this module.
+    E.g. where i2c bus numbering or GPIO pin definitions change. """
     try:
         with open('/proc/cpuinfo','r') as f:
             for line in f:
                 if line.startswith('Revision'):
-                    code = line.rstrip()[-4:]
-                    if code in ['0002', '0003']:
-                        return 1
-                    elif code in ['0010', '0012']:
-                        # Attempting to catch the A+/B+. Technically wrong number,
-                        # but I don't know what to put considering they are rev 1
-                        # with conflicts with the 1st rev B's...
-                        return 3
+                    code = int(line.rstrip()[-4:], 16)
+                    if code <= 3:
+                        return 1 # PCB rev 1.0, model B, 256MB, default i2c bus 0, 26 pin
+                    elif code < 16:
+                        return 2 # PCB rev 2.0, model A/B, default i2c bus 1, 26 pin
                     else:
-                        return 2
+                        return 3 # A+/B+/Model 2 B, 40 pin
             else:
                 return 0
     except:
         return 0
+
